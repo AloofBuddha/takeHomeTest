d847c1fe512f0c7c3a613c188fef2f1516c4f2dc
Benjamin Cohen <benjamin@codeplatoon.org>
2025-12-25 11:01:22 -0300

    created home page and general data grid

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,30 @@
-import TradesTable from '@/components/TradesTable'
 import { createFileRoute } from '@tanstack/react-router'
+import ConfigurableTable from '@/components/ConfigurableTable'
 import tradesData from '@/data/homePage/trades.json'
 import { TRADES_COLUMN_DEFS } from '@/data/columnDefs'
+import { TRADES_TABLE_COLOR_CONFIG } from '@/config/tableConfigs'
 
 export const Route = createFileRoute('/')({
 	component: RouteComponent
 })
 
 function RouteComponent() {
+	const processedColumnDefs = TRADES_COLUMN_DEFS.map((col) => {
+		const processed = { ...col }
+		if (['id', 'accountId', 'currency', 'lastUpdate'].includes(col.field || '')) {
+			processed.initialHide = true
+		}
+		return processed
+	})
+
 	return (
 		<div className="h-[calc(100vh-12rem)]">
-			<TradesTable trades={tradesData} columnDefs={TRADES_COLUMN_DEFS} />
+			<ConfigurableTable
+				data={tradesData}
+				columnDefs={processedColumnDefs}
+				tableId="trades"
+				colorModeConfig={TRADES_TABLE_COLOR_CONFIG}
+			/>
 		</div>
 	)
 }
